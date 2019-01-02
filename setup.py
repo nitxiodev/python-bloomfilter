f03220b27e29e084b957bdb54fcbbefbf19a7205
nitxiodev <smnitxio@gmail.com>
2019-01-02 01:05:09 +0100

    updating setup.py adding dependencies

--- setup.py
+++ setup.py
@@ -13,6 +13,16 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/nitxiodev/python-bloomfilter",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'redis==2.10.6',
+        'psutil==5.4.8',
+        'mock==2.0.0',
+        'mmh3==2.5.1',
+        'bitarray==0.8.3',
+        'numpy==1.15.4',
+        'PyHamcrest==1.9.0',
+        'mockredispy==2.9.3'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
